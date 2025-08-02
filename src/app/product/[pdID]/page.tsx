6d51f99803f92047daf8afbd579a3bfd4d2cb432
ACER_ASPIRE_5\vinak <vinakasi1214@gmail.com>
2025-08-02 22:55:16 +0700

    Save2

--- src/app/product/[pdID]/page.tsx
+++ src/app/product/[pdID]/page.tsx
@@ -2,10 +2,12 @@ import NavbarSite from "@/components/navbarSite";
 import ProductDetailPage from "@/components/ProductDetail";
 
 interface Props {
-  params: { pdID: string };
+//   params: { pdID: string };
 }
 
-export default async function DetailPage({ params }: Props) {
+export default async function ProductPage() {
+//   const { pdID } = params;
+
   return (
     <div>
       <NavbarSite />
